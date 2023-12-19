1ddfa335271a984fb96991c3cac47e4d68906b96
Alan Burke <alan@cycletraffic.com>
2023-12-19 21:15:15 +0000

    Add settings files - attempt to read db

--- web/sites/default/settings.upsun.php
+++ web/sites/default/settings.upsun.php
@@ -6,9 +6,9 @@
  */
 
 $driver = "mysql";
-$databases['default']['default']['database'] = getenv(DB_DATABASE);
-$databases['default']['default']['username'] = getenv(DB_USERNAME);
-$databases['default']['default']['password'] = getenv(DB_PASSWORD);
-$databases['default']['default']['host'] = getenv(DB_HOST);
-$databases['default']['default']['port'] = getenv(DB_PORT);
+$databases['default']['default']['database'] = getenv('DB_DATABASE');
+$databases['default']['default']['username'] = getenv('DB_USERNAME');
+$databases['default']['default']['password'] = getenv('DB_PASSWORD');
+$databases['default']['default']['host'] = getenv('DB_HOST');
+$databases['default']['default']['port'] = getenv('DB_PORT');
 $databases['default']['default']['driver'] = $driver;
